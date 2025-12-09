10757b7d8feda949021f144953f5e179b9b7c6c9
Deny <denysetiapratama@gmail.com>
2025-12-09 11:55:45 +0800

    menambahkan export data

--- index.php
+++ index.php
@@ -88,6 +88,7 @@
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
                             Tambah Barang
                         </button>
+                        <a href="export.php" class="btn btn-info">Export Data</a>
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple">
